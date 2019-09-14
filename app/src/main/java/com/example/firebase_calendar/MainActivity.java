aa48bae9bca9bbfe49abd0d7c6fe4f2c49a9918b
mli25782 <mli25782@gmail.com>
2019-09-14 12:42:44 -0400

    I made a pretty calendar list view

--- app/src/main/java/com/example/firebase_calendar/MainActivity.java
+++ app/src/main/java/com/example/firebase_calendar/MainActivity.java
@@ -9,28 +9,71 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.CalendarView;
+import android.widget.ListView;
 
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewEvent;
+import com.github.naz013.awcalendar.AwesomeCalendarView;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
 
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Objects;
 
+import hirondelle.date4j.DateTime;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    MaterialCalendarView materialCalendarView;
-
+//    MaterialCalendarView materialCalendarView;
+    AwesomeCalendarView awesomeCalendarView;
+    String[] timeArray = {"12:00am", "", "", "", "1:00am", "", "", "", "2:00am", "", "", "", "3:00am", "", "", "",
+                        "4:00am", "", "", "", "5:00am", "", "", "", "6:00am", "", "", "", "7:00am", "", "", "",
+                        "8:00am", "", "", "", "9:00am", "", "", "", "10:00am", "", "", "", "11:00am", "", "", "",
+                        "12:00pm", "", "", "", "1:00pm", "", "", "", "2:00pm", "", "", "", "3:00pm", "", "", "",
+                        "4:00pm", "", "", "", "5:00pm", "", "", "", "6:00pm", "", "", "", "7:00pm", "", "", "",
+                        "8:00pm", "", "", "", "9:00pm", "", "", "", "10:00pm", "", "", "", "11:00pm", "", "", "", "12:00am"};
+    String[] eventArray = {"Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event",
+                            "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event",
+                            "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event",
+                            "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event",
+                            "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event",
+                            "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event",
+                            "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event",
+                            "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event", "Event",};
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        awesomeCalendarView = (AwesomeCalendarView) findViewById(R.id.calendar_view);
+        CalendarAdapter calendarAdapter = new CalendarAdapter(this, timeArray, eventArray);
+        listView = (ListView) findViewById(R.id.list_view);
+        listView.setAdapter(calendarAdapter);
+
+        awesomeCalendarView.setOnDateClickListener(new AwesomeCalendarView.OnDateClickListener() {
+            @Override
+            public void onDateClicked(DateTime dateTime) {
+                Log.d(TAG, "onDateClicked: " + dateTime);
+            }
+        });
+        awesomeCalendarView.setOnCurrentMonthListener(new AwesomeCalendarView.OnCurrentMonthListener() {
+            @Override
+            public void onMonthSelected(int year, int month) {
+                Log.d(TAG, "onMonthSelected: " + year + "-" + month);
+            }
+        });
+        awesomeCalendarView.setOnDateLongClickListener(new AwesomeCalendarView.OnDateLongClickListener() {
+            @Override
+            public void onDateLongClicked(DateTime dateTime) {
+                Log.d(TAG, "onDateLongClicked: " + dateTime);
+            }
+        });
 //        WeekView mWeekView;
 //        WeekView.EventClickListener mEventClickListener;
 //        MonthLoader.MonthChangeListener mMonthChangeListener;
@@ -40,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
 //        materialCalendarView.setOnDateChangedListener(new OnDateSelectedListener() {
 //            @Override
 //            public void onDateSelected(@NonNull MaterialCalendarView widget, @NonNull CalendarDay date, boolean selected) {
-//
+//                Log.v(TAG, "Clicked");
 //            }
 //        });
 // Get a reference for the week view in the layout.
