09723e72f229e1763520efb3bfc1627c17c16316
dmanikt <dmanikt@gmail.com>
2019-09-14 06:59:37 -0400

    swap email & id

--- app/src/main/java/com/example/firebase_calendar/models/User.java
+++ app/src/main/java/com/example/firebase_calendar/models/User.java
@@ -2,15 +2,23 @@ package com.example.firebase_calendar.models;
 
 import com.google.firebase.database.IgnoreExtraProperties;
 
+import java.util.ArrayList;
+
 @IgnoreExtraProperties
 public class User {
     public String email;
     public String id;
+    public ArrayList<String> friends;
 
     public User() {}
 
-    public User(String email, String id) {
+    public User(String id, String email) {
         this.email = email;
         this.id = id;
+        this.friends = new ArrayList<>();
+    }
+
+    public void addFriend(String id) {
+        friends.add(id);
     }
 }
