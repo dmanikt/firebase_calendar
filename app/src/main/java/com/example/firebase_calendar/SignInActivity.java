a4ee2596a72906d6be2da33582767e3f11a60fc8
dmanikt <dmanikt@gmail.com>
2019-09-14 05:32:09 -0400

    sign in auth

--- app/src/main/java/com/example/firebase_calendar/SignInActivity.java
+++ app/src/main/java/com/example/firebase_calendar/SignInActivity.java
@@ -8,12 +8,12 @@ import com.firebase.ui.auth.AuthUI;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class SignInActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
 
         List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
         startActivityForResult(
