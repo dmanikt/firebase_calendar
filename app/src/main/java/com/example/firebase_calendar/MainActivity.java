497124896ce8a850dec8ef8b681a4dda42871a4e
dmanikt <dmanikt@gmail.com>
2019-09-14 04:30:39 -0400

    add email auth

--- app/src/main/java/com/example/firebase_calendar/MainActivity.java
+++ app/src/main/java/com/example/firebase_calendar/MainActivity.java
@@ -3,11 +3,23 @@ package com.example.firebase_calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers).build(),
+                123);
     }
 }
